95ef4e4e8c5dfaa5c0267532a89598c119d9e176
minbanyartalahtaw <minbanyartalahtaww@gmail.com>
2024-11-28 14:55:39 +0700

    week-003 --1

--- CSX-java/Class-Teaching/004/src/intro_array/array004.java
+++ CSX-java/Class-Teaching/004/src/intro_array/array004.java
@@ -1,4 +1,5 @@
 package intro_array;
+import java.util.ArrayList;
 
 public class array004 {
     public static int max(int[] num_data) {
@@ -29,20 +30,49 @@ public class array004 {
         return total;
     }
 
-    public static int[] even(int[] num_data) {
-        int[] evenNumber = new int[num_data.length];
+    // This line of code is important
+    public static ArrayList<Integer> even(int[] num_data) {
+        ArrayList<Integer> evenNumber = new ArrayList<Integer>();
         for (int i = 0; i < num_data.length;i++) {
             if (num_data[i] % 2 == 0 ) {
-                evenNumber[i] = num_data[i];
+                evenNumber.add(num_data[i]);
             };
         };
         return evenNumber;
     };
+
+    // This line of code is important too
+    public static ArrayList<String> identify(int[] num_data) {
+        ArrayList<String> result = new ArrayList<String>();
+        for (int i = 0; i < num_data.length;i++) {
+            if (num_data[i] % 2 == 0 ) {
+                result.add("Even");
+            } else {
+                result.add("Odd");
+            }
+        };
+        return result;
+    }
+
+    //reverse the array
+    public static ArrayList<Integer> reverse(int[] num_data) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int i = num_data.length - 1; i > -1;i--) {
+            result.add(num_data[i]);
+        }
+        return result;
+    }
+
+
+
+
     public static void main(String[] args) {
         int[] num_data = {100,469,782,605,243,712,402};
         System.out.println("The max number is : " + max(num_data));
         System.out.println("The min number is : " + min(num_data));
         System.out.println("The sum number is : " + sum(num_data));
         System.out.println("The even number is : " + even(num_data));
+        System.out.println("The identify number is : " + identify(num_data));
+        System.out.println("The reverse number is : " + reverse(num_data));
     }
 }
---
+++ CSX-java/Class-Teaching/004/src/intro_array/array005Important.java
@@ -0,0 +1,18 @@
+package intro_array;
+import java.util.ArrayList;
+public class array005Important {
+    public static void main(String[] args) {
+        ArrayList<Integer> myData = new ArrayList<Integer>();
+        myData.add(1);
+        myData.add(2);
+        myData.add(3);
+        myData.add(4);
+        myData.add(5);
+        myData.add(6);
+        myData.add(7);
+        myData.add(8);
+        myData.add(9);
+        myData.add(10);
+        System.out.println(myData);
+    }
+}
